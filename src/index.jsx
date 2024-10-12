c0c335f9ea1fd60dcd994357de24e08ed735aef9
mahrokhnz <mahrokh.nz@gmail.com>
2024-10-12 20:48:11 +0330

    feat: init commit

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import ColorsGame from './pages';
+import './index.css';
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<ColorsGame />} />
+      </Routes>
+    </HashRouter>
+  );
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import './index.css';
+import React from 'react';
+
+function ColorsGame() {
+  return (
+    <main className="colorsGame">
+      hi
+    </main>
+  );
+}
+
+export default ColorsGame;
